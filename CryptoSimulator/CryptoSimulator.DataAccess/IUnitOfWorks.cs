d74f85b39dc002c7d4ed345a5e8a88f2608773bd
Aneta Stankovska <anetastankovskaane@gmail.com>
2022-09-22 12:44:58 +0200

    Updated project structure

---
+++ CryptoSimulator/CryptoSimulator.DataAccess/IUnitOfWorks.cs
@@ -0,0 +1,18 @@
+﻿using CryptoSimulator.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoSimulator.DataAccess
+{
+    public interface IUnitOfWorks<TContext> where TContext : IDbContext, new()
+    {
+        IRepository<T> GetGenericRepository<T>()
+          where T : class;
+        void SaveChanges();
+        public void Dispose();
+        IDbContext ReturnContext();
+    }
+}
