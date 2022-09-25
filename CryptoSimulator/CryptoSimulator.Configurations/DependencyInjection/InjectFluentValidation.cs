1d07ed00cfb032511d10f66c599a9636901a9973
IvanovskiStefan <96203470+IvanovskiStefan@users.noreply.github.com>
2022-09-25 18:48:53 +0200

    Register validation completed

---
+++ CryptoSimulator/CryptoSimulator.Configurations/DependencyInjection/InjectFluentValidation.cs
@@ -0,0 +1,17 @@
+﻿using CryptoSimulator.Common.Validator;
+using CryptoSimulator.ServiceModels.UserModels;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CryptoSimulator.Configurations.DependencyInjection
+{
+    public static class InjectFluentValidation
+    {
+        public static IServiceCollection RegisterFluentValidation(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<RegisterUser>, RegisterUserValidator>();
+
+            return services;
+        }
+    }
+}
